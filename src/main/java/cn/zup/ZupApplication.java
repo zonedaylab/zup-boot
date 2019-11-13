a233e608cc91648a2134db18680f8897b4078c9d
samsons199 <1124330015@qq.com>
2019-11-13 10:58:25 +0800

    EnableSwagger2

--- src/main/java/cn/zup/ZupApplication.java
+++ src/main/java/cn/zup/ZupApplication.java
@@ -9,13 +9,19 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @ImportResource("classpath:zup-*.xml")
 @ComponentScan({"cn.zup.*"})
+@ServletComponentScan
+@EnableSwagger2
 public class ZupApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ZupApplication.class, args);
 	}
+	
+	
 }
