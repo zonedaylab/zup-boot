70397b3cddd55f15c87df2954162becc9711a781
liuxinfengabc@163.com <624310862@qq.com>
2020-02-16 20:57:43 +0800

    BI的js文件，CSS样式文件在业务系统里有，更新到zup中

---
+++ src/main/webapp/plug-in/ace/bi/js/loading.js
@@ -0,0 +1,24 @@
+/**
+ * Created by andot on  2018-5-31 16:48:00
+ */
+var Loading = function(){
+	/**
+	 * color:如果不填写显示白色，填入颜色值，就显示对应颜色
+	 * */
+	Loading.prototype.start = function(color) {
+		var index = layer.load(0, {
+			skin: 'i-loading',
+			shade: 0.1,
+			content: '<div class="sk-wave"><div class="sk-rect sk-rect1"></div><div class="sk-rect sk-rect2"></div><div class="sk-rect sk-rect3"></div><div class="sk-rect sk-rect4"></div><div class="sk-rect sk-rect5"></div></div>'
+		});
+		if(typeof(color) != "undefined")
+			$(".sk-rect").css("background-color", color);
+		return index;
+    }
+	/**
+	 * index: 要关闭弹窗的实例
+	 * */
+	Loading.prototype.stop = function(index) {
+    	layer.close(index);
+    }
+}
