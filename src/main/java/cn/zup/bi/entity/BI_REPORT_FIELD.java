cf0734b2c4aed9d4577f641725b2583750f9047d
zhenghui <8042965@qq.com>
2020-05-04 08:00:35 +0800

    去掉主题表，把主题表合并到报表

--- src/main/java/cn/zup/bi/entity/BI_REPORT_FIELD.java
+++ src/main/java/cn/zup/bi/entity/BI_REPORT_FIELD.java
@@ -7,10 +7,25 @@ import java.io.Serializable;
 @Data
 public class BI_REPORT_FIELD implements Serializable {
 	private Integer report_Field_Id;
-	private Integer report_Id;
-	private Integer field_Id;
-	private Integer field_Location;
-	private Integer dim_Order;
+	private Integer report_Id; //报表ID
+	private Integer field_Id; //字段位置
+	private Integer field_Location;//字段位置
+	private Integer dim_Order; //维度顺序
 	private Integer display; //字段是否显示
-	private String field_Name;
+
+	private String field_Name;//字段名称
+
+	//新加
+	private String field_Caption;//字段别名
+	private String field_Title;//字段标题
+	private Integer data_Type;//数据类型（数据库类型）
+	private Integer field_Type;//字段类型（业务类型）
+	private String aggregate_Type;//聚合类型
+	private String unit;//单位
+	private Integer dim_Id;//维表ID
+
+
+	private String data_Type_Str;
+	private String field_Type_Str;
+
 }
